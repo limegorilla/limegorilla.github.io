b4f8abe0a5eaa6f789c2176906ac8fbf8bf9b097
Liam Doyle <limegorilla@gmail.com>
2019-12-16 00:14:33 +0000

    Ok, other than other pages it fucking works

--- assets/scripts/scripts.js
+++ assets/scripts/scripts.js
@@ -6,3 +6,11 @@ function toggleNav() {
     x.style.display = "none";
   }
 }
+function toggleInfo() {
+  var x = document.getElementById("info");
+  if (x.style.display === "none") {
+    x.style.display = "flex";
+  } else {
+    x.style.display = "none";
+  }
+}
